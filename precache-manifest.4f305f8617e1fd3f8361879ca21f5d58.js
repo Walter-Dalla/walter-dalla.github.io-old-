118f3af8516a2b33af76074e435252749250090c
Walter <wwddttnn@hotmail.com>
2020-03-19 00:42:15 -0300

    primeiro commit par ao github pages
    Signed-off-by: Walter <wwddttnn@hotmail.com>

---
+++ precache-manifest.4f305f8617e1fd3f8361879ca21f5d58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f786a5a968b0a33df4a68d5bdf66dfb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9e4f42722e3349e33b",
+    "url": "/static/css/main.e466e53b.chunk.css"
+  },
+  {
+    "revision": "7ec1e495ef46374420f9",
+    "url": "/static/js/2.43140a1d.chunk.js"
+  },
+  {
+    "revision": "d175e66b535ceea2ddfb324336bbe01c",
+    "url": "/static/js/2.43140a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9e4f42722e3349e33b",
+    "url": "/static/js/main.027be34b.chunk.js"
+  },
+  {
+    "revision": "20026e1d7a24d28c2b28",
+    "url": "/static/js/runtime-main.eed6e83d.js"
+  },
+  {
+    "revision": "2d274ac267295805b7dc46bc9201acc8",
+    "url": "/static/media/inicio.2d274ac2.jpg"
+  },
+  {
+    "revision": "e816a99d6c33974b914408f240aaf5c2",
+    "url": "/static/media/unespLogo.e816a99d.png"
+  },
+  {
+    "revision": "8999bc94fb3d1d238e8d822723fe86d5",
+    "url": "/static/media/unicampLogo.8999bc94.png"
+  }
+]);
